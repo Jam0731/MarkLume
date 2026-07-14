ba23d4f4bc20ab6a8aefc8e5589e4422570caae5
夏皓天 <Jam0731@users.noreply.github.com>
2026-07-14 16:22:31 +0800

    fix: remove vite-plugin-singlefile from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
-export default defineConfig(({ mode }) => {
-  const isSingleFile = mode === 'single'
-
-  return {
-    plugins: [
-      vue(),
-      ...(isSingleFile ? [viteSingleFile()] : [])
-    ],
-    build: {
-      outDir: isSingleFile ? 'public' : 'dist',
-      cssCodeSplit: false,
-      assetsInlineLimit: 100000000
-    }
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    cssCodeSplit: false
   }
 })
